64982914ec197e222cd02713558fc766808ecd9e
AkshayGera <akshay16gera@gmail.com>
2022-03-13 19:56:54 -0500

    depth first and breadth first graph traversal

--- Graphs/Graph.js
+++ Graphs/Graph.js
@@ -21,14 +21,91 @@ class Graph {
         }
         delete this.adjacencyList[vertex];
     }
+    dfsRecursive(start) {
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex) {
+            if (!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    return dfs(neighbor);
+                }
+            })
+        })(start);
+
+        return result;
+    }
+    dfsIterative(start) {
+        const stack = [];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        stack.push(start);
+        visited[start] = true;
+
+        while (stack.length) {
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
+            });
+        }
+        return result;
+    }
+    bfs(start) {
+        const result = [];
+        const visited = {};
+
+        const queue = [start];
+        let currentVertex;
+        visited[start] = true;
+        while (queue.length) {
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            });
+        }
+        return result;
+    }
 }
 let g = new Graph();
-g.addVertex("Tokyo");
-g.addVertex("Dallas");
-g.addVertex("Aspen");
-g.addEdge("Tokyo", "Dallas");
-g.addEdge("Aspen", "Dallas");
-g.addEdge("Tokyo", "Aspen");
-g.removeEdge("Dallas", "Aspen");
-g.removeVertex("Tokyo");
+// g.addVertex("Tokyo");
+// g.addVertex("Dallas");
+// g.addVertex("Aspen");
+// g.addEdge("Tokyo", "Dallas");
+// g.addEdge("Aspen", "Dallas");
+// g.addEdge("Tokyo", "Aspen");
+// g.removeEdge("Dallas", "Aspen");
+// g.removeVertex("Tokyo");
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+console.log(g.dfsRecursive("A"));
+console.log(g.dfsIterative("A"));
+console.log(g.bfs("A"));
 console.log(JSON.stringify(g.adjacencyList));
